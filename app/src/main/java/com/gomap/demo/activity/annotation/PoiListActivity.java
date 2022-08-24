1f8b4a8add2aef28f885461775e5f2634c62e59f
xuming.liu <xuming.liu@g42.ai>
2022-08-24 14:55:57 +0400

    feature:fix error

--- app/src/main/java/com/gomap/demo/activity/annotation/PoiListActivity.java
+++ app/src/main/java/com/gomap/demo/activity/annotation/PoiListActivity.java
@@ -1,9 +1,12 @@
 package com.gomap.demo.activity.annotation;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.PointF;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -22,12 +25,20 @@ import com.gomap.sdk.camera.CameraPosition;
 import com.gomap.sdk.camera.CameraUpdateFactory;
 import com.gomap.sdk.geometry.LatLng;
 import com.gomap.sdk.geometry.LatLngBounds;
+import com.gomap.sdk.location.LocationComponent;
+import com.gomap.sdk.location.LocationComponentActivationOptions;
+import com.gomap.sdk.location.engine.LocationEngineCallback;
+import com.gomap.sdk.location.engine.LocationEngineResult;
+import com.gomap.sdk.location.permissions.PermissionsListener;
+import com.gomap.sdk.location.permissions.PermissionsManager;
 import com.gomap.sdk.maps.MapView;
 import com.gomap.sdk.maps.MapboxMap;
 import com.gomap.sdk.maps.Style;
 import com.gomap.sdk.poi.PoiService;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.Type;
 import java.text.DecimalFormat;
@@ -36,28 +47,48 @@ import java.util.List;
 
 public class PoiListActivity extends AppCompatActivity {
 
+    private PermissionsManager permissionsManager;
+
     private MapView mapView;
     private MapboxMap mapboxMap;
     private ArrayList<MarkerOptions> markerList = new ArrayList<>();
 
     private static final DecimalFormat LAT_LON_FORMATTER = new DecimalFormat("#.#####");
-    private final LatLng CENTER = new LatLng(24.4628,54.3697);
+    private final LatLng CENTER = new LatLng(24.4628, 54.3697);
 
     private static String STATE_MARKER_LIST = "markerList";
 
 
-    private View search ;
+    private View search;
 
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poi_list);
-
         search = findViewById(R.id.txt_search_poi);
         mapView = (MapView) findViewById(R.id.mapView);
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            mapView.onCreate(savedInstanceState);
+            initMap();
+        } else {
+            permissionsManager = new PermissionsManager(new PermissionsListener() {
 
-        mapView.onCreate(savedInstanceState);
-        initMap();
+                @Override
+                public void onExplanationNeeded(List<String> list) {
+                }
+
+                @Override
+                public void onPermissionResult(boolean granted) {
+                    if (granted) {
+                        mapView.onCreate(savedInstanceState);
+                        initMap();
+                    } else {
+                        finish();
+                    }
+                }
+            });
+            permissionsManager.requestLocationPermissions(this);
+        }
 
 
         findViewById(R.id.txt_find_center).setOnClickListener(new View.OnClickListener() {
@@ -82,47 +113,11 @@ public class PoiListActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 LatLng center = findCenter();
-
-                PoiService.getInstance().requestPoi(center,500,PoiListActivity.this,new PoiService.NetCallBack() {
+                Location location = mapboxMap.getLocationComponent().getLastKnownLocation();
+                PoiService.getInstance().requestNearbyPoi(new LatLng(location),center, 500, PoiListActivity.this, new PoiService.NetCallBack() {
                     @Override
                     public void onCallBack(String response) {
-                        Type type = new TypeToken<HttpResponse<MoreResponse<PoiModel>> >() {
-                        }.getType();
-                        HttpResponse<MoreResponse<PoiModel>> httpResponse =  new Gson().fromJson(response,type);
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mapboxMap.clear();
-                                List<PoiModel> list = httpResponse.getData().getList();
-
-                                if (list != null && !list.isEmpty()){
-                                    ArrayList<Point> listPoint  = new ArrayList<Point>();
-                                    Marker marker = null;
-                                    for (PoiModel poiModel:
-                                            list) {
-                                        marker = addMarker(new LatLng(Double.parseDouble(poiModel.getLat()),Double.parseDouble(poiModel.getLng())));
-
-                                        listPoint.add(Point.fromLngLat(Double.parseDouble(poiModel.getLng()),Double.parseDouble(poiModel.getLat())));
-                                    }
-//                                    if (marker != null){
-//                                        mapboxMap.selectMarker(marker);
-//                                    }
-                                    // poi 显示在一定范围内
-                                    List<List<Point>> polygonDefinition = new ArrayList<List<Point>>() {
-                                        {
-                                            add(listPoint);
-                                        }
-                                    };
-                                    CameraPosition actualPosition = mapboxMap.getCameraForGeometry(Polygon.fromLngLats(polygonDefinition));
-
-                                    mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(actualPosition));
-
-                                }else {
-                                    Toast.makeText(PoiListActivity.this,"no data",Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                        handlePoiResult(response);
 
                     }
                 });
@@ -130,30 +125,92 @@ public class PoiListActivity extends AppCompatActivity {
         });
     }
 
+    private void handlePoiResult(String response) {
+        Type type = new TypeToken<HttpResponse<MoreResponse<PoiModel>>>() {
+        }.getType();
+        HttpResponse<MoreResponse<PoiModel>> httpResponse = new Gson().fromJson(response, type);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mapboxMap.clear();
+                List<PoiModel> list = httpResponse.getData().getList();
+
+                if (list != null && !list.isEmpty()) {
+                    ArrayList<Point> listPoint = new ArrayList<Point>();
+                    Marker marker = null;
+                    for (PoiModel poiModel :
+                            list) {
+                        marker = addMarker(new LatLng(Double.parseDouble(poiModel.getLat()), Double.parseDouble(poiModel.getLng())));
+
+                        listPoint.add(Point.fromLngLat(Double.parseDouble(poiModel.getLng()), Double.parseDouble(poiModel.getLat())));
+                    }
+//                                    if (marker != null){
+//                                        mapboxMap.selectMarker(marker);
+//                                    }
+                    // poi 显示在一定范围内
+                    List<List<Point>> polygonDefinition = new ArrayList<List<Point>>() {
+                        {
+                            add(listPoint);
+                        }
+                    };
+                    CameraPosition actualPosition = mapboxMap.getCameraForGeometry(Polygon.fromLngLats(polygonDefinition));
+
+                    mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(actualPosition));
+
+                } else {
+                    Toast.makeText(PoiListActivity.this, "no data", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
     private LatLng findCenter() {
 
         int height = ScreenUtils.getScreenHeight(PoiListActivity.this);
         int width = ScreenUtils.getScreenWidth(PoiListActivity.this);
 
-        return mapboxMap.getProjection().fromScreenLocation(new PointF(width/2 ,height/2));
-
+        return mapboxMap.getProjection().fromScreenLocation(new PointF(width / 2, height / 2));
     }
-
-    private void initMap(){
+    @SuppressLint("MissingPermission")
+    private void initMap() {
         mapView.getMapAsync(map -> {
             mapboxMap = map;
             resetMap();
 
-            mapboxMap.setStyle(Style.getPredefinedStyle("Streets"));
+            mapboxMap.setStyle(Style.getPredefinedStyle("Streets"), new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull @NotNull Style style) {
 
-            CameraPosition cameraPosition = new CameraPosition.Builder()
-                    .target(CENTER)
-                    .zoom(12)
-                    .tilt(30)
-                    .tilt(0)
-                    .build();
-            mapboxMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                    LocationComponent component = mapboxMap.getLocationComponent();
+                    component.activateLocationComponent(
+                            LocationComponentActivationOptions
+                                    .builder(PoiListActivity.this, style)
+                                    .useDefaultLocationEngine(true)
+                                    .build()
+                    );
+                    component.setLocationComponentEnabled(true);
+                    component.getLocationEngine().getLastLocation(new LocationEngineCallback<LocationEngineResult>() {
+                        @SuppressLint("MissingPermission")
+                        @Override
+                        public void onSuccess(LocationEngineResult result) {
 
+                            if (!mapView.isDestroyed()) mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(result.getLastLocation()), 12.0));
+                        }
+
+                        @Override
+                        public void onFailure(@NonNull @NotNull Exception e) {
+
+                        }
+                    });
+                }
+            });
         });
     }
 
