426d55038ca5d1736a0ead78f2b5f9e0c11b6321
dong.jin <dong.jin@g42.ai>
2022-12-13 17:27:37 +0400

    add bearing test

--- app/src/main/java/com/gomap/demo/activity/annotation/BulkMarkerActivity.java
+++ app/src/main/java/com/gomap/demo/activity/annotation/BulkMarkerActivity.java
@@ -1,10 +1,16 @@
 package com.gomap.demo.activity.annotation;
 
+import static com.gomap.sdk.style.layers.Property.ICON_ROTATION_ALIGNMENT_MAP;
+import static com.gomap.sdk.style.layers.PropertyFactory.iconAllowOverlap;
+import static com.gomap.sdk.style.layers.PropertyFactory.iconImage;
+import static com.gomap.sdk.style.layers.PropertyFactory.iconRotationAlignment;
+
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.gomap.demo.R;
@@ -21,6 +27,8 @@ import com.gomap.sdk.style.layers.Property;
 import com.gomap.sdk.style.layers.PropertyFactory;
 import com.gomap.sdk.style.layers.SymbolLayer;
 import com.gomap.sdk.style.sources.GeoJsonSource;
+import com.gomap.sdk.utils.BitmapUtils;
+import com.gomap.turf.TurfMeasurement;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -36,7 +44,7 @@ public class BulkMarkerActivity extends AppCompatActivity {
     private MapboxMap mapboxMap;
     private MapView mapView;
     private List<LatLng> locations = new ArrayList<>();
-
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,18 +65,155 @@ public class BulkMarkerActivity extends AppCompatActivity {
 
         mapboxMap.clear();
 
-        locations.add(new LatLng(38.898356214229885,-76.95071680042268));
-        locations.add(new LatLng(38.96196109243133,-77.00388711500801));
-        locations.add(new LatLng(38.92682257867399,-77.00697621720708));
-        locations.add(new LatLng(38.87947513411545,-76.93706903318301));
-        locations.add(new LatLng(38.87756015531194,-76.99833925795618));
-        locations.add(new LatLng(38.920826177353554,-77.00344143825373));
-        locations.add(new LatLng(38.94973736942091,-77.09691804556762));
-        locations.add(new LatLng(38.88373832686895,-77.0323420800206));
-        locations.add(new LatLng(38.95882519049787,-77.01701177841085));
-        locations.add(new LatLng(38.89196180216161,-77.02376546437307));
+        locations.add(new LatLng(38.1008356214229885,-76.30195071680042268));
+        locations.add(new LatLng(38.10196196109243133,-76.31195071680042268));
+
+        locations.add(new LatLng(38.10496196109243133,-76.31295071680042268));
+        locations.add(new LatLng(38.10596196109243133,-76.31395071680042268));
+        locations.add(new LatLng(38.10696196109243133,-76.31495071680042268));
+        locations.add(new LatLng(38.10796196109243133,-76.31595071680042268));
+        locations.add(new LatLng(38.10896196109243133,-76.31695071680042268));
+        locations.add(new LatLng(38.10996196109243133,-76.31795071680042268));
+        locations.add(new LatLng(38.11096196109243133,-76.31895071680042268));
+        locations.add(new LatLng(38.11196196109243133,-76.31995071680042268));
+        locations.add(new LatLng(38.11296196109243133,-76.32095071680042268));
+        locations.add(new LatLng(38.11396196109243133,-76.32195071680042268));
+        locations.add(new LatLng(38.11496196109243133,-76.32295071680042268));
+        locations.add(new LatLng(38.11596196109243133,-76.32395071680042268));
+        locations.add(new LatLng(38.11696196109243133,-76.32495071680042268));
+
+        locations.add(new LatLng(38.11796196109243133,-76.33495071680042268));
+        locations.add(new LatLng(38.11896196109243133,-76.34495071680042268));
+        locations.add(new LatLng(38.11996196109243133,-76.35495071680042268));
+        locations.add(new LatLng(38.120169619610924313,-76.36495071680042268));
+        locations.add(new LatLng(38.12196196109243133,-76.37495071680042268));
+        locations.add(new LatLng(38.12296196109243133,-76.38495071680042268));
+        locations.add(new LatLng(38.12396196109243133,-76.39495071680042268));
+        locations.add(new LatLng(38.12496196109243133,-76.40495071680042268));
+        locations.add(new LatLng(38.12596196109243133,-76.41495071680042268));
+        locations.add(new LatLng(38.12696196109243133,-76.42495071680042268));
+        locations.add(new LatLng(38.12796196109243133,-76.43495071680042268));
+        locations.add(new LatLng(38.12896196109243133,-76.4495071680042268));
+        locations.add(new LatLng(38.12996196109243133,-76.45495071680042268));
+        locations.add(new LatLng(38.13096196109243133,-76.46495071680042268));
+
+        locations.add(new LatLng(38.13196196109243133,-76.47495071680042268));
+        locations.add(new LatLng(38.13296196109243133,-76.48495071680042268));
+        locations.add(new LatLng(38.13396196109243133,-76.49495071680042268));
+        locations.add(new LatLng(38.134169619610924313,-76.50495071680042268));
+        locations.add(new LatLng(38.13596196109243133,-76.5195071680042268));
+        locations.add(new LatLng(38.13696196109243133,-76.52495071680042268));
+        locations.add(new LatLng(38.13796196109243133,-76.53495071680042268));
+        locations.add(new LatLng(38.13896196109243133,-76.54495071680042268));
+        locations.add(new LatLng(38.13996196109243133,-76.55495071680042268));
+        locations.add(new LatLng(38.14096196109243133,-76.56495071680042268));
+        locations.add(new LatLng(38.14196196109243133,-76.57495071680042268));
+        locations.add(new LatLng(38.14296196109243133,-76.5895071680042268));
+        locations.add(new LatLng(38.14396196109243133,-76.59495071680042268));
+        locations.add(new LatLng(38.14496196109243133,-76.60495071680042268));
+
+        locations.add(new LatLng(38.14596196109243133,-76.61495071680042268));
+        locations.add(new LatLng(38.14696196109243133,-76.62495071680042268));
+        locations.add(new LatLng(38.14796196109243133,-76.63495071680042268));
+        locations.add(new LatLng(38.148169619610924313,-76.64495071680042268));
+        locations.add(new LatLng(38.14996196109243133,-76.6595071680042268));
+        locations.add(new LatLng(38.15096196109243133,-76.66495071680042268));
+        locations.add(new LatLng(38.15196196109243133,-76.67495071680042268));
+        locations.add(new LatLng(38.15296196109243133,-76.68495071680042268));
+        locations.add(new LatLng(38.15396196109243133,-76.69495071680042268));
+        locations.add(new LatLng(38.15496196109243133,-76.70495071680042268));
+        locations.add(new LatLng(38.15596196109243133,-76.71495071680042268));
+        locations.add(new LatLng(38.15696196109243133,-76.7295071680042268));
+        locations.add(new LatLng(38.15796196109243133,-76.73495071680042268));
+        locations.add(new LatLng(38.15896196109243133,-76.74495071680042268));
+        locations.add(new LatLng(38.15996196109243133,-76.75495071680042268));
+        locations.add(new LatLng(38.16096196109243133,-76.76495071680042268));
+        locations.add(new LatLng(38.16196196109243133,-76.77495071680042268));
+        locations.add(new LatLng(38.162169619610924313,-76.78495071680042268));
+        locations.add(new LatLng(38.16396196109243133,-76.7995071680042268));
+        locations.add(new LatLng(38.16496196109243133,-76.80495071680042268));
+        locations.add(new LatLng(38.16596196109243133,-76.81495071680042268));
+        locations.add(new LatLng(38.16696196109243133,-76.82495071680042268));
+        locations.add(new LatLng(38.16796196109243133,-76.83495071680042268));
+        locations.add(new LatLng(38.16896196109243133,-76.84495071680042268));
+        locations.add(new LatLng(38.16996196109243133,-76.85495071680042268));
+        locations.add(new LatLng(38.17096196109243133,-76.8695071680042268));
+        locations.add(new LatLng(38.17196196109243133,-76.8795071680042268));
+        locations.add(new LatLng(38.17296196109243133,-76.880495071680042268));
+
+        locations.add(new LatLng(38.30596196109243133,-76.61495071680042268));
+        locations.add(new LatLng(38.31696196109243133,-76.62495071680042268));
+        locations.add(new LatLng(38.32796196109243133,-76.63495071680042268));
+        locations.add(new LatLng(38.338169619610924313,-76.64495071680042268));
+        locations.add(new LatLng(38.34996196109243133,-76.6595071680042268));
+        locations.add(new LatLng(38.35096196109243133,-76.66495071680042268));
+        locations.add(new LatLng(38.36196196109243133,-76.67495071680042268));
+        locations.add(new LatLng(38.37296196109243133,-76.68495071680042268));
+        locations.add(new LatLng(38.38396196109243133,-76.69495071680042268));
+        locations.add(new LatLng(38.39496196109243133,-76.70495071680042268));
+        locations.add(new LatLng(38.40596196109243133,-76.71495071680042268));
+        locations.add(new LatLng(38.41696196109243133,-76.7295071680042268));
+        locations.add(new LatLng(38.42796196109243133,-76.73495071680042268));
+        locations.add(new LatLng(38.43896196109243133,-76.74495071680042268));
+        locations.add(new LatLng(38.44996196109243133,-76.75495071680042268));
+        locations.add(new LatLng(38.45096196109243133,-76.76495071680042268));
+        locations.add(new LatLng(38.46196196109243133,-76.77495071680042268));
+        locations.add(new LatLng(38.472169619610924313,-76.78495071680042268));
+        locations.add(new LatLng(38.4896196109243133,-76.7995071680042268));
+        locations.add(new LatLng(38.49496196109243133,-76.80495071680042268));
+        locations.add(new LatLng(38.50596196109243133,-76.81495071680042268));
+        locations.add(new LatLng(38.51696196109243133,-76.82495071680042268));
+        locations.add(new LatLng(38.52796196109243133,-76.83495071680042268));
+        locations.add(new LatLng(38.53896196109243133,-76.84495071680042268));
+        locations.add(new LatLng(38.54996196109243133,-76.85495071680042268));
+        locations.add(new LatLng(38.56096196109243133,-76.8695071680042268));
+        locations.add(new LatLng(38.58196196109243133,-76.8795071680042268));
+        locations.add(new LatLng(38.60296196109243133,-76.880495071680042268));
+
+        mapboxMap.moveCamera(locations.get(0),11f,500);
 
         showGlMarkers();
+
+        showAirIcon();
+    }
+
+    private Point lastPoint;
+    private void showAirIcon() {
+        mapboxMap.getStyle(style -> {
+            style.addImage("air_icon",
+                    BitmapUtils.getBitmapFromDrawable(getResources().getDrawable(R.drawable.ic_airplanemode_active_black_24dp)));
+            LatLng latlng = locations.remove(0);
+            lastPoint = Point.fromLngLat(latlng.getLongitude(),latlng.getLatitude());
+            GeoJsonSource geoJsonSource = new GeoJsonSource("air_icon_source",lastPoint);
+            style.addSource(geoJsonSource);
+            SymbolLayer symbolLayer = new SymbolLayer("bearing_icon_test","air_icon_source");
+            symbolLayer.withProperties(
+                    iconImage("air_icon"),
+                    iconAllowOverlap(true),
+                    iconRotationAlignment(ICON_ROTATION_ALIGNMENT_MAP));
+            style.addLayer(symbolLayer);
+
+            handler = new Handler(Looper.myLooper());
+            handler.postDelayed(() -> {
+                emitLocation();
+            }, 1000);
+
+        });
+    }
+
+    private synchronized void emitLocation() {
+        if (locations.size() > 0){
+            LatLng latlng = locations.remove(0);
+            Point nextPoint = Point.fromLngLat(latlng.getLongitude(),latlng.getLatitude());
+            double bearing = TurfMeasurement.bearing(lastPoint,nextPoint);
+            mapboxMap.getStyle().getLayer("bearing_icon_test").setProperties(PropertyFactory.iconRotate((float) bearing));
+            lastPoint = nextPoint;
+            GeoJsonSource geoJsonSource = mapboxMap.getStyle().getSourceAs("air_icon_source");
+            geoJsonSource.setGeoJson(nextPoint);
+            handler.postDelayed(() -> {
+                emitLocation();
+            }, 1000);
+        }
     }
 
     private void showGlMarkers() {
@@ -101,13 +246,13 @@ public class BulkMarkerActivity extends AppCompatActivity {
                     new LineLayer("route-line-layer", "route-line-source").withProperties(
                             PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
                             PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
-                            PropertyFactory.lineWidth(10f),
+                            PropertyFactory.lineWidth(5f),
                             PropertyFactory.lineColor(Color.BLUE),
                             PropertyFactory.iconImage("oneway"),
                             PropertyFactory.iconSize(15.0f),
                             PropertyFactory.symbolSpacing(5.0f),
                             PropertyFactory.iconOpacity(0.5f),
-                            PropertyFactory.iconRotationAlignment(Property.ICON_ROTATION_ALIGNMENT_MAP),
+                            iconRotationAlignment(ICON_ROTATION_ALIGNMENT_MAP),
                             PropertyFactory.symbolPlacement(Property.SYMBOL_PLACEMENT_LINE)
                     )
             );
@@ -121,17 +266,17 @@ public class BulkMarkerActivity extends AppCompatActivity {
                             PropertyFactory.iconRotate(90.0f),
                             PropertyFactory.iconImage("icon_sample_test"),
                             PropertyFactory.symbolSpacing(10.0f),//间距
-                            PropertyFactory.iconRotationAlignment(Property.ICON_ROTATION_ALIGNMENT_MAP),
+                            iconRotationAlignment(ICON_ROTATION_ALIGNMENT_MAP),
                             PropertyFactory.symbolPlacement(Property.SYMBOL_PLACEMENT_LINE)
                     )
             );
 
         });
-//        Float[] dashArray ={4.0f,1.5f};
-//        IconFactory iconFactory = IconFactory.getInstance(this);
-//        Source testSource = mapboxMap.getStyle().getSource("openmaptiles");
-//        List<Layer> tmpList = mapboxMap.getStyle().getLayers();
-//        Layer testLayer = mapboxMap.getStyle().getLayer("road_motorway");
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
 }
